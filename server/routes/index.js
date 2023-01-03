30e5dfe0df6c193ae12e2cda4555fa4012cea671
Sean Wallace <sean.wallace.australia@gmail.com>
2023-01-04 09:35:01 +1100

    bug fix: routes/index.js sendFile changed from /client/build/index.html to /client/public/index.html

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,8 @@ router.use('/api', apiRoutes);
 
 // serve up react front-end in production
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  // res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/public/index.html'));
 });
 
 module.exports = router;
